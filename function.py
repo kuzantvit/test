c19571edda35ba9088e89aadfe8965dab5f97c7e
kuzantvit <kuzantvit@gmail.com>
2017-09-05 23:34:49 +0300

    minor

---
+++ function.py
@@ -0,0 +1,10 @@
+def favourite_book(title):
+    print("My favourite book is " + title.title())
+favourite_book('Dark tower')
+c = 0
+def summa(a, b):
+    c = a + b
+    return c
+summa(1, 4)
+print(summa(2, 5))
+print(summa(6, 5))
